a013ab99fff26c36770ffc4bdd2d35ee50f59f5a
Aniko Gerstweiler <gerstweilera@gmail.com>
2014-07-13 00:06:46 +0200

    add one test case to MessageCodecTest

--- src/test/java/com/epam/training/server/MessageCodecTest.java
+++ src/test/java/com/epam/training/server/MessageCodecTest.java
@@ -1,15 +1,21 @@
 package com.epam.training.server;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+
+import com.epam.training.message.Size;
 
 public class MessageCodecTest {
 
@@ -18,22 +24,27 @@ public class MessageCodecTest {
 	}
 
 	@Test
-	public void testDecodeWhenInputIsSizeReturnsSizeMessage() {
+	public void testDecodeWhenInputIsSizeReturnsSizeMessage() throws Exception {
 		//given
 		MessageCodec codec = new MessageCodec();
-
+		final byte[] src = "SIZE 1 1".getBytes("UTF8");
 		//when
 		ChannelHandlerContext ctx = Mockito.mock(ChannelHandlerContext.class);
 		ByteBuf in = Mockito.mock(ByteBuf.class);
-		Mockito.when(in.readBytes(Mockito.any(byte[].class))).then(new Answer<ByteBuf>() {
+		when(in.readableBytes()).thenReturn(src.length);
+		when(in.readBytes(Mockito.any(byte[].class))).then(new Answer<ByteBuf>() {
 			@Override
 			public ByteBuf answer(InvocationOnMock invocation) throws Throwable {
 				Object[] args = invocation.getArguments();
+				byte[] msg = (byte[]) args[0];
+				System.arraycopy(src , 0, msg, 0, src.length - 1);
 				return null;
 			}
 		});
-		List<Object> out;
-		//codec.decode(ctx, in, out);
+		List<Object> out = new ArrayList<>();
+		codec.decode(ctx, in, out);
+		assertEquals(1, out.size());
+		Assert.assertEquals(Size.class, out.get(0).getClass());
 	}
 
 }
